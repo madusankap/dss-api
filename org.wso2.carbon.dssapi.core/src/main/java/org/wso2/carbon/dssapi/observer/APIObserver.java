adb7a744e84d9a2f54ea8896015a6cf42df502d1
Madusanka <rmmpremaratne@gmail.com>
2015-05-17 11:28:14 +0530

    imports optimized

--- org.wso2.carbon.dssapi.core/src/main/java/org/wso2/carbon/dssapi/observer/APIObserver.java
+++ org.wso2.carbon.dssapi.core/src/main/java/org/wso2/carbon/dssapi/observer/APIObserver.java
@@ -1,3 +1,19 @@
+/*
+* Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.wso2.carbon.dssapi.observer;
 
 import org.apache.axiom.om.OMElement;
@@ -13,7 +29,6 @@ import org.apache.axis2.engine.AxisObserver;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.dataservices.common.DBConstants;
-import org.wso2.carbon.dataservices.core.DBUtils;
 import org.wso2.carbon.dataservices.core.admin.DataServiceAdmin;
 import org.wso2.carbon.dataservices.core.engine.DataService;
 import org.wso2.carbon.dataservices.ui.beans.Data;
@@ -24,12 +39,10 @@ import org.wso2.carbon.service.mgt.ServiceAdmin;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamWriter;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class APIObserver implements AxisObserver {
     private static final Log log = LogFactory.getLog(APIObserver.class);
@@ -39,44 +52,44 @@ public class APIObserver implements AxisObserver {
     public void init(AxisConfiguration axisConfiguration) {
     }
 
-     @Override
+    @Override
     public void serviceUpdate(AxisEvent axisEvent, AxisService axisService) {
 
-         if(axisEvent.getEventType()==AxisEvent.SERVICE_DEPLOY){
-             DataService dataService = (DataService) axisService.getParameter(DBConstants.DATA_SERVICE_OBJECT).getValue();
-                 if (dataService != null) {
-                     String location = dataService.getDsLocation();
-                     location = location.substring(0, location.lastIndexOf("/"));
-                     File file = new File(location + "/" + dataService.getName() + APIUtil.APPLICATION_XML);
-                     if (file.exists()) {
-                         Application application;
-                         try {
+        if (axisEvent.getEventType() == AxisEvent.SERVICE_DEPLOY) {
+            DataService dataService = (DataService) axisService.getParameter(DBConstants.DATA_SERVICE_OBJECT).getValue();
+            if (dataService != null) {
+                String location = dataService.getDsLocation();
+                location = location.substring(0, location.lastIndexOf("/"));
+                File file = new File(location + "/" + dataService.getName() + APIUtil.APPLICATION_XML);
+                if (file.exists()) {
+                    Application application;
+                    try {
 
-                             JAXBContext jaxbContext = JAXBContext.newInstance(Application.class);
-                             Unmarshaller jaxbUnMarshaller = jaxbContext.createUnmarshaller();
-                             application = (Application) jaxbUnMarshaller.unmarshal(file);
-                             String serviceContents = new DataServiceAdmin().getDataServiceContentAsString(dataService.getName());
-                             InputStream ins = new ByteArrayInputStream(serviceContents.getBytes());
-                             OMElement configElement = (new StAXOMBuilder(ins)).getDocumentElement();
-                             configElement.build();
-                             Data data = new Data();
-                             data.populate(configElement);
-                             String tempDeployedTime=new ServiceAdmin(DataHolder.getConfigurationContext().getAxisConfiguration()).getServiceData(data.getName()).getServiceDeployedTime();
-                             if(!application.getDeployedTime().equalsIgnoreCase(tempDeployedTime)){
-                                 new APIUtil().updateApi(dataService.getName(), application.getUserName(), application.getTenantDomain(), data, application.getVersion());
-                                 Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-                                 jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-                                 application.setDeployedTime(tempDeployedTime);
-                                 jaxbMarshaller.marshal(application, file);
-                             }
-                         }  catch (Exception e) {
-                             log.error("Couldn't get ServiceMetaData Object for the service"+dataService.getName(),e);
-                         }
+                        JAXBContext jaxbContext = JAXBContext.newInstance(Application.class);
+                        Unmarshaller jaxbUnMarshaller = jaxbContext.createUnmarshaller();
+                        application = (Application) jaxbUnMarshaller.unmarshal(file);
+                        String serviceContents = new DataServiceAdmin().getDataServiceContentAsString(dataService.getName());
+                        InputStream ins = new ByteArrayInputStream(serviceContents.getBytes());
+                        OMElement configElement = (new StAXOMBuilder(ins)).getDocumentElement();
+                        configElement.build();
+                        Data data = new Data();
+                        data.populate(configElement);
+                        String tempDeployedTime = new ServiceAdmin(DataHolder.getConfigurationContext().getAxisConfiguration()).getServiceData(data.getName()).getServiceDeployedTime();
+                        if (!application.getDeployedTime().equalsIgnoreCase(tempDeployedTime)) {
+                            new APIUtil().updateApi(dataService.getName(), application.getUserName(), application.getTenantDomain(), data, application.getVersion());
+                            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+                            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+                            application.setDeployedTime(tempDeployedTime);
+                            jaxbMarshaller.marshal(application, file);
+                        }
+                    } catch (Exception e) {
+                        log.error("Couldn't get ServiceMetaData Object for the service" + dataService.getName(), e);
+                    }
 
-                     }
+                }
 
-                 }
-             }
+            }
+        }
 
     }
 
